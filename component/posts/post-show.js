4fd87b721544dc1a927c5a9f96c715c0be612b22
enZo <inbox.huytran@gmail.com>
2020-08-26 16:47:54 +0700

    update time

--- component/posts/post-show.js
+++ component/posts/post-show.js
@@ -82,7 +82,7 @@ export default class PostShow extends React.Component {
                                     <h4 className="mb-1">{postContent && postContent.fields.postByName}</h4>
                                     <p className="card-text small text-muted"> 
                                         <span className="fe fe-clock mr-2"></span> 
-                                        <time dateTime="2018-05-24">{postContent && postContent.fields.createdAt}</time>
+                                        <time dateTime="2018-05-24">{postContent && postContent.fields.createdAt ? new Date(postContent.fields.createdAt).toDateString():null}</time>
                                     </p>
                                 </div>
                             </div>
@@ -94,44 +94,42 @@ export default class PostShow extends React.Component {
                         <p className="text-center mb-3">
                             { postContent && postContent.fields.photos
                             ? 
-                            <>
-                                <img src={postContent.fields.photos[0].url} alt="..." className="img-fluid rounded"/>
-                                <div className="mb-3">
-                                    <div className="row">
-                                        <div className="col">
-                                            <span className="btn btn-sm btn-white mr-2">
-                                                👍 {postContent && postContent.fields.like}
-                                            </span>
-                                            <a href="#!" className="btn btn-sm btn-white">
-                                                😬 {postContent && postContent.fields.dislike}
-                                            </a>
-                                            
-                                        </div>
-                                        {/* <div className="col-auto mr-n3">
-                                            <div className="avatar-group d-none d-sm-flex">
-                                                <a href="profile-posts.html" className="avatar avatar-xs" data-toggle="tooltip" title="" data-original-title="Ab Hadley">
-                                                    <img src="/assets/img/avatars/profiles/avatar-2.jpg" alt="..." className="avatar-img rounded-circle"/>
-                                                </a>
-                                                <a href="profile-posts.html" className="avatar avatar-xs" data-toggle="tooltip" title="" data-original-title="Adolfo Hess">
-                                                    <img src="/assets/img/avatars/profiles/avatar-3.jpg" alt="..." className="avatar-img rounded-circle"/>
-                                                </a>
-                                                <a href="profile-posts.html" className="avatar avatar-xs" data-toggle="tooltip" title="" data-original-title="Daniela Dewitt">
-                                                    <img src="/assets/img/avatars/profiles/avatar-4.jpg" alt="..." className="avatar-img rounded-circle"/>
-                                                </a>
-                                                <a href="profile-posts.html" className="avatar avatar-xs" data-toggle="tooltip" title="" data-original-title="Miyah Myles">
-                                                    <img src="/assets/img/avatars/profiles/avatar-5.jpg" alt="..." className="avatar-img rounded-circle"/>
-                                                </a>
-                                            </div>
-                                        </div> */}
-                                        {/* <div className="col-auto">
-                                            <a href="#!" className="btn btn-sm btn-white">Share</a>
-                                        </div> */}
-                                    </div>
-                                </div>
-                            </>
+                            <img src={postContent.fields.photos[0].url} alt="..." className="img-fluid rounded"/>
                             : null
                             }
                         </p>
+                        <div className="mb-3">
+                            <div className="row">
+                                <div className="col">
+                                    <span className="btn btn-sm btn-white mr-2">
+                                        👍 {postContent && postContent.fields.like}
+                                    </span>
+                                    <a href="#!" className="btn btn-sm btn-white">
+                                        😬 {postContent && postContent.fields.dislike}
+                                    </a>
+                                    
+                                </div>
+                                {/* <div className="col-auto mr-n3">
+                                    <div className="avatar-group d-none d-sm-flex">
+                                        <a href="profile-posts.html" className="avatar avatar-xs" data-toggle="tooltip" title="" data-original-title="Ab Hadley">
+                                            <img src="/assets/img/avatars/profiles/avatar-2.jpg" alt="..." className="avatar-img rounded-circle"/>
+                                        </a>
+                                        <a href="profile-posts.html" className="avatar avatar-xs" data-toggle="tooltip" title="" data-original-title="Adolfo Hess">
+                                            <img src="/assets/img/avatars/profiles/avatar-3.jpg" alt="..." className="avatar-img rounded-circle"/>
+                                        </a>
+                                        <a href="profile-posts.html" className="avatar avatar-xs" data-toggle="tooltip" title="" data-original-title="Daniela Dewitt">
+                                            <img src="/assets/img/avatars/profiles/avatar-4.jpg" alt="..." className="avatar-img rounded-circle"/>
+                                        </a>
+                                        <a href="profile-posts.html" className="avatar avatar-xs" data-toggle="tooltip" title="" data-original-title="Miyah Myles">
+                                            <img src="/assets/img/avatars/profiles/avatar-5.jpg" alt="..." className="avatar-img rounded-circle"/>
+                                        </a>
+                                    </div>
+                                </div> */}
+                                <div className="col-auto">
+                                    {/* <a href="#!" className="btn btn-sm btn-white">Share</a> */}
+                                </div>
+                            </div>
+                        </div>
 
                         <hr/>
                         <div className="comment mb-3">
